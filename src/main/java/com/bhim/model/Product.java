a5a12a6c11747c08ab8aaa0a4e2b4dd566a3c0e8
bheemalinga-kuruva <javabybhim1996@gmail.com>
2024-10-04 14:17:47 +0530

    config file add  for customize the security

---
+++ src/main/java/com/bhim/model/Product.java
@@ -0,0 +1,45 @@
+package com.bhim.model;
+
+public class Product {
+
+	private Integer productId;
+	private String productName;
+	private Double productPrice;
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public Product(Integer productId, String productName, Double productPrice) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.productPrice = productPrice;
+	}
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/com/bhim/rest/WelComeRestController.java
+++ src/main/java/com/bhim/rest/WelComeRestController.java
@@ -3,6 +3,8 @@ package com.bhim.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bhim.model.Product;
+
 @RestController
 public class WelComeRestController {
 
@@ -11,4 +13,17 @@ public class WelComeRestController {
 		String msg="Wel come to Andhra Pradesh";
 		return msg;
 	}
+	@GetMapping("/contact")
+	public String getContact() {
+		Long  contact=9876543210L;
+		return "contact number"+contact;
+	}
+	@GetMapping("/products")
+	public Product getProducts() {
+	Product product= new Product();
+	product.setProductId(101);
+	product.setProductName("laptop");
+	product.setProductPrice(249.0);
+	return product;
+}
 }
