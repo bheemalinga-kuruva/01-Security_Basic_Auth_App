83c4d8f2d1d7f3f61bb5f9894ece2aeaba13c382
bheemalinga-kuruva <javabybhim1996@gmail.com>
2024-10-04 18:03:21 +0530

    customizing the security for users login

--- src/main/java/com/bhim/config/SecurityConfig.java
+++ src/main/java/com/bhim/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,6 +14,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	
+	@Bean
+	public InMemoryUserDetailsManager configureUsers() {
+			
+		   UserDetails adminUser = User.withDefaultPasswordEncoder()
+										.username("bheema")
+										.password("bheema@123")
+										.authorities("ADMIN")
+										.build();
+			
+			UserDetails normalUser = User.withDefaultPasswordEncoder()
+										.username("rahul")
+										.password("rahul@123")
+										.authorities("USER")
+										.build();
+			
+			return new InMemoryUserDetailsManager(adminUser, normalUser);
+	}
+
 	@Bean
 	public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception{
 		
